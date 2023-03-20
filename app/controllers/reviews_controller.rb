721cc60cd01a241d7e3cbf8fbaf2eb1a59753421
Glen <glen.kimani@student.moringaschool.com>
2023-03-20 16:41:08 +0300

    Completed

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -2,7 +2,7 @@ class ReviewsController < ApplicationController
 
   def index
     reviews = Review.all.order(rating: :desc)
-    render json: reviews
+    render json: reviews, innclude: :dog_house
   end
 
 end
